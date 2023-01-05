efc099dbb12066aee78c949b637dad58024ac840
sergiosentry <109162568+sergiosentry@users.noreply.github.com>
2023-01-05 12:25:50 -0500

    Binding custom transaction to the current scope

--- EmpowerPlant/CartViewController.swift
+++ EmpowerPlant/CartViewController.swift
@@ -46,13 +46,13 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
     func purchase() {
         let transaction = SentrySDK.startTransaction(
           name: "purchase",
-          operation: "http.client" // 'http.client' appears automatically in our js app, don't have to set 'operation' // this param is mandatory
+          operation: "http.client" ,// 'http.client' appears automatically in our js app, don't have to set 'operation' // this param is mandatory
+          bindToScope: true // needed for distributed tracing - It binds a transaction to the scope making it accessible to every method running
         )
 
         // use localhost for development against dev-backend
         // let url = URL(string: "http://127.0.0.1:8080/checkout")!
         let url = URL(string: "https://application-monitoring-flask-dot-sales-engineering-sf.appspot.com/checkout")!
-
 
         var request = URLRequest(url: url)
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
